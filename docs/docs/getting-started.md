663a9ac43cc9b61b5231325833b4a35cbb026ee1
maddhruv <dhruvjainpenny@gmail.com>
2020-02-04 20:01:34 +0530

    build: use tailwindcss

--- docs/docs/getting-started.md
+++ docs/docs/getting-started.md
@@ -6,6 +6,10 @@ sidebar_label: Getting Started
 
 ## Install
 
+### Font
+
+`<link href="https://fonts.googleapis.com/css?family=DM+Sans:400,700&display=swap" rel="stylesheet">`
+
 ## Usage
 
 ```html live
---
+++ tailwind.config.js
@@ -0,0 +1,85 @@
+module.exports = {
+  theme: {
+    colors: {
+      blue: {
+        100: "#7FB7FE",
+        200: "#2F89FC",
+        300: "#185EB8"
+      },
+      green: {
+        100: "#7FE3AD",
+        200: "#2BCF76",
+        300: "#169451"
+      },
+      orange: {
+        100: "#FFC194",
+        200: "#FE9D54",
+        300: "#B9642A"
+      },
+      grey: {
+        100: "#CFD4DD",
+        200: "#ADB1BB",
+        300: "#8B8F99"
+      },
+      white: "#ffffff",
+      black: "#000000"
+    },
+    spacing: {
+      sm: "4px",
+      md: "8px",
+      lg: "16px",
+      xl: "24px"
+    },
+    borderRadius: {
+      sm: "2px",
+      md: "4px",
+      lg: "8px",
+      full: "50%"
+    },
+    borderWidth: {
+      sm: "1px",
+      md: "2px",
+      lg: "3px",
+      xl: "4px"
+    },
+    boxShadow: {
+      sm: "0 3px 8px rgba(70, 74, 85, 0.16)",
+      md: "0 8px 13px rgba(70, 74, 85, 0.16)"
+    },
+    cursor: {
+      pointer: "pointer",
+      text: "text",
+      "not-allowed": "not-allowed"
+    },
+    fill: false,
+    fontFamily: false,
+    fontSize: {
+      xs: ".75rem",
+      sm: ".875rem",
+      base: "1rem",
+      lg: "1.25rem",
+      xl: "1.5rem",
+      "2xl": "2rem"
+    },
+    fontWeight: {
+      light: 300,
+      normal: 400,
+      bold: 700
+    },
+    letterSpacing: false,
+    lineHeight: {
+      none: 1,
+      tight: 1.25,
+      loose: 1.75
+    },
+    opacity: false,
+    stroke: false,
+    zIndex: {
+      0: 0,
+      10: 10,
+      20: 20
+    }
+  },
+  variants: {},
+  plugins: []
+};
