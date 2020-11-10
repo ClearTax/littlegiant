c2bf804460dc7ed567079aede2f4e0ab3b051809
Anshul Pinto <70994472+anshul-pinto0410@users.noreply.github.com>
2020-11-10 15:49:46 +0530

    feat: product card ca section 3 (#41)
    * feat: product card ca section 3
    
    * feat: resolved comments
    
    Co-authored-by: anshul-pinto0410 <anshul-pinto0410@github.com>

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -20,7 +20,7 @@ module.exports = {
       {
         type: "category",
         label: "Cards",
-        items: ["cards","carousel-card", "faq-card", "presscard"],
+        items: ["cards","carousel-card", "faq-card", "presscard","product-card"],
       },
       "colors",
       "cursor",
